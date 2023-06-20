d6b8c77836c30a61dc9690d60d02f2e62d5b0596
unnamed-ii <isaingashev@gmail.com>
2023-06-20 17:54:09 +0300

    created and added container component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import './home.scss';
+import Container from "../../components/container/container";
 
 const Home = () => {
     return (
         <section className="home">
-            <div className="home__info">
-                <div className="home__info-title">
-                    Front-End React Developer
+            <Container>
+                <div className="home__info">
+                    <div className="home__info-title">
+                        Front-End React Developer
+                    </div>
+                    <div className="home__info-text">
+                        Hi, I'm Isa Ingashev. A passionate front-end developer based in Izmir / Turkey
+                    </div>
+                    <ul className="home__info-links">
+                        <li><a href="#">LinkedIn</a></li>
+                        <li><a href="#">GitHub</a></li>
+                        <li><a href="#">LeetCode</a></li>
+                        <li><a href="#">CodeWars</a></li>
+                    </ul>
+                    <ul className="home__info-stack">
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>React</li>
+                    </ul>
                 </div>
-                <div className="home__info-text">
-                    Hi, I'm Isa Ingashev. A passionate front-end developer based in Izmir / Turkey
-                </div>
-                <ul className="home__info-links">
-                    <li><a href="#">LinkedIn</a></li>
-                    <li><a href="#">GitHub</a></li>
-                    <li><a href="#">LeetCode</a></li>
-                    <li><a href="#">CodeWars</a></li>
-                </ul>
-                <ul className="home__info-stack">
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                </ul>
-            </div>
+            </Container>
         </section>
     );
 };
