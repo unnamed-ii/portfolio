871317a5721d1d33f67b128cf7fd85d180de2095
unnamed-ii <isaingashev@gmail.com>
2023-07-03 19:09:45 +0300

    added styles for contacts block

--- src/blocks/ContactMe/Contacts.jsx
+++ src/blocks/ContactMe/Contacts.jsx
@@ -1,36 +1,37 @@
 import React from 'react';
 import './contacts.scss';
 import Container from "../../components/container/container";
+import {ReactComponent as GmailIcon} from "../../images/icons/gmail.svg";
+import {ReactComponent as TelegramIcon} from "../../images/icons/telegram.svg";
+import {ReactComponent as LinkedInIcon} from "../../images/icons/linkedin.svg";
 
 const Contacts = () => {
     return (
         <section className="contacts" id="contacts-section">
             <Container>
-                <div className="contacts__headline">Contacts</div>
-                <div className="contacts__title">You can contact me any time</div>
-                <div className="contacts__list">
-                    <div className="contacts__list-contact">
-                        <img src="#" alt="icon" className="contacts__list-contact__image"/>
-                        <div className="contacts__list-contact__box">
-                            <div className="title">
-                                Telegram
-                            </div>
-                            <a href="#" className="link">
-                                https://t.me/unnamedii
+                <div className="contacts__inner">
+                    <div className="contacts__inner-headline">Contacts</div>
+                    <div className="contacts__inner-title">Contact me any time</div>
+                    <ul className="contacts__inner-list">
+                        <li className="contacts__inner-list__contact">
+                            <a href="https://t.me/unnamedii" className="link">
+                                <TelegramIcon />
                             </a>
-                        </div>
-                    </div>
-                    <div className="contacts__list-contact">
-                        <img src="#" alt="icon" className="contacts__list-contact__image"/>
-                        <div className="contacts__list-contact__box">
-                            <div className="title">
-                                Mail
-                            </div>
-                            <a href="#" className="link">
-                                isaingashev@gmail.com
+                            <div className="text">https://t.me/unnamedii</div>
+                        </li>
+                        <li className="contacts__inner-list__contact">
+                            <a href="mailto:isaingashev@gmail.com" className="link">
+                                <GmailIcon />
                             </a>
-                        </div>
-                    </div>
+                            <div className="text">isaingashev@gmail.com</div>
+                        </li>
+                        <li className="contacts__inner-list__contact">
+                            <a href="https://www.linkedin.com/in/isa-ingashev/" className="link">
+                                <LinkedInIcon />
+                            </a>
+                            <div className="text">https://www.linkedin.com/in/isa-ingashev/</div>
+                        </li>
+                    </ul>
                 </div>
             </Container>
         </section>
