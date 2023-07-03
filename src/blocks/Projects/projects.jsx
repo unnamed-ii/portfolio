08e1e1c58b2fca532a3e51a3ab4f4f21212a858e
unnamed-ii <isaingashev@gmail.com>
2023-07-03 19:37:03 +0300

    edited projects blocks layount and styles

--- src/blocks/Projects/projects.jsx
+++ src/blocks/Projects/projects.jsx
@@ -1,15 +1,42 @@
 import React from 'react';
 import './projects.scss';
 import Container from "../../components/container/container";
+import {ReactComponent as LinkToIcon} from "../../images/icons/link.svg";
 
-const ProjectsData = [];
+const ProjectsData = [
+    {
+        image: "",
+        title: "Wawe",
+        text: "\"Wawe\" is a surfing school where you can learn how surf easily with the help of the rop surfers in the world",
+        linkToProject: "linkToProject",
+        linkToCode: "linkToCode",
+    },
+];
 
-const Project = ({image, title, text}) => {
+const Project = ({image, title, text, linkToProject, linkToCode}) => {
     return (
         <div className="project">
             <img src={image} alt="ImageOfProject" className="project__image"/>
-            <h1 className="project__title">{title}</h1>
-            <p className="project__text">{text}</p>
+            <div className="project__box">
+                <div className="project__box-info">
+                    <h1 className="project__box-info__title">{title}</h1>
+                    <p className="project__box-info__text">{text}</p>
+                </div>
+                <ul className="project__box-links">
+                    <li>
+                        <a href={linkToProject}>
+                            <LinkToIcon className="icon"/>
+                            <h4>Go to "{title}"</h4>
+                        </a>
+                    </li>
+                    <li>
+                        <a href={linkToCode}>
+                            <LinkToIcon className="icon"/>
+                            <h4>Source code</h4>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
@@ -20,14 +47,21 @@ const Projects = () => {
             <Container>
                 {ProjectsData.length
                     ?
-                    <div className="projects__list">
-                        {ProjectsData.map(project =>
-                            <Project
-                                image={project.images}
-                                title={project.title}
-                                text={project.text}
-                            />
-                        )}
+                    <div className="projects__inner">
+                        <div className="projects__inner-title">
+                            Projects I've done
+                        </div>
+                        <div className="projects__inner-list">
+                            {ProjectsData.map(project =>
+                                <Project
+                                    image={project.image}
+                                    title={project.title}
+                                    text={project.text}
+                                    linkToProject={project.linkToProject}
+                                    linkToCode={project.linkToCode}
+                                />
+                            )}
+                        </div>
                     </div>
                     :
                     <div className="projects__notfound">
